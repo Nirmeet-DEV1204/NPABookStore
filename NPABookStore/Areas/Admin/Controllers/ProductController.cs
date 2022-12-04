bb0acfc795b83739cd64bb5b503fcd80a01fcc04
Nirmeet-DEV1204 <dev.nirmeet07mail.com>
2022-12-04 18:32:42 -0500

    Error LIst Solved

--- NPABookStore/Areas/Admin/Controllers/ProductController.cs
+++ NPABookStore/Areas/Admin/Controllers/ProductController.cs
@@ -65,38 +65,38 @@ namespace NPABookStore.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-                string webRootPath = _hostEnvironment.WebRootPath;
-                var files = HttpContext.Request.Form.Files;
-                if (files.Count > 0)
-                {
-                    string fileName = Guid.NewGuid().ToString();
-                    var uploads = Path.Combine(webRootPath, @"images\products");
-                    var extension = Path.GetExtension(files[0].FileName);
+                //string webRootPath = _hostEnvironment.WebRootPath;
+                //var files = HttpContext.Request.Form.Files;
+                //if (files.Count > 0)
+                //{
+                //    string fileName = Guid.NewGuid().ToString();
+                //    var uploads = Path.Combine(webRootPath, @"images\products");
+                //    var extension = Path.GetExtension(files[0].FileName);
 
-                    if (productVM.Product.ImageUrl != null)
-                    {
-                        // this is an edit and we need to remove old image
-                        var imagePath = Path.Combine(webRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
-                        if (System.IO.File.Exists(imagePath))
-                        {
-                            System.IO.File.Delete(imagePath);
-                        }
-                    }
-                    using (var filesStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
-                    {
-                        files[0].CopyTo(filesStreams);
-                    }
-                    productVM.Product.ImageUrl = @"\images\products\" + fileName + extension;
-                }
-                else
-                {
-                    // update when they do not change the image
-                    if (productVM.Product.Id != 0)
-                    {
-                        Product objFromDb = _unitOfWork.Product.Get(productVM.Product.Id);
-                        productVM.Product.ImageUrl = objFromDb.ImageUrl;
-                    }
-                }
+                //    if (productVM.Product.ImageUrl != null)
+                //    {
+                //        // this is an edit and we need to remove old image
+                //        var imagePath = Path.Combine(webRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+                //        if (System.IO.File.Exists(imagePath))
+                //        {
+                //            System.IO.File.Delete(imagePath);
+                //        }
+                //    }
+                //    using (var filesStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                //    {
+                //        files[0].CopyTo(filesStreams);
+                //    }
+                //    productVM.Product.ImageUrl = @"\images\products\" + fileName + extension;
+                //}
+                //else
+                //{
+                //    // update when they do not change the image
+                //    if (productVM.Product.Id != 0)
+                //    {
+                //        Product objFromDb = _unitOfWork.Product.Get(productVM.Product.Id);
+                //        productVM.Product.ImageUrl = objFromDb.ImageUrl;
+                //    }
+                //}
 
                 if (productVM.Product.Id == 0)
                 {
@@ -147,11 +147,11 @@ namespace NPABookStore.Areas.Admin.Controllers
                 return Json(new { success = false, message = "Error while deleting" });
             }
             string webRootPath = _hostEnvironment.WebRootPath;
-            var imagePath = Path.Combine(webRootPath, objFromDb.ImageUrl.TrimStart('\\'));
-            if (System.IO.File.Exists(imagePath))
-            {
-                System.IO.File.Delete(imagePath);
-            }
+            //var imagePath = Path.Combine(webRootPath, objFromDb.ImageUrl.TrimStart('\\'));
+            //if (System.IO.File.Exists(imagePath))
+            //{
+            //    System.IO.File.Delete(imagePath);
+            //}
             _unitOfWork.Product.Remove(objFromDb);
             _unitOfWork.Save();
             return Json(new { success = true, message = "Delete Successful" });
